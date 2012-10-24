2de56cc3f8e2c05cea1e5c7fa897b5a2447f8335
Joshua Rieken <joshua@joshuarieken.com>
2012-10-23 21:45:24 -0500

    Input handling for all four quadrants

--- Assets/Code/Frog.cs
+++ Assets/Code/Frog.cs
@@ -3,12 +3,47 @@ using System.Collections;
 
 public class Frog : MonoBehaviour {
 	
-	public float _speed;
+	public float speed;
+	
 	private Vector3 startPosition;
+	private float startSpeed;
+	
+	private Rect upperLeftBounds;
+	private Rect lowerLeftBounds;
+	private Rect upperRightBounds;
+	private Rect lowerRightBounds;
+	
+	private enum MoveState {
+		Floating,
+		Charging,
+		Boosting
+	}
+	private MoveState moveState;
+	
+	private float Speed {
+		get {
+			switch (moveState) {
+			case MoveState.Boosting:
+				return 15;
+			case MoveState.Floating:
+				return 5;
+			case MoveState.Charging:
+				return 0;
+			default:
+				return 0;
+			}
+		}
+	}
 	
 	#region MonoBehaviour
 	void Awake() {
 		startPosition = transform.localPosition;
+		startSpeed = speed;
+		
+		lowerLeftBounds = new Rect(0, 0, Screen.width/2, Screen.height/2);
+		upperLeftBounds = new Rect(0, Screen.height/2, Screen.width/2, Screen.height/2);
+		lowerRightBounds = new Rect(Screen.width/2, 0, Screen.width/2, Screen.height/2);
+		upperRightBounds = new Rect(Screen.width/2, Screen.height/2, Screen.width/2, Screen.height/2);
 	}
 
 	// Use this for initialization
@@ -18,7 +53,8 @@ public class Frog : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + _speed * Time.deltaTime);
+		MoveForward();
+		HandleInput();
 	}
 	
 	void OnCollisionEnter(Collision collision) {
@@ -26,6 +62,24 @@ public class Frog : MonoBehaviour {
     }
 	#endregion
 	
+	void MoveForward() {
+		transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + Speed * Time.deltaTime);
+	}
+	
+	void HandleInput() {
+		if (Input.GetButtonDown("Fire1")) {
+	        if (upperLeftBounds.Contains(Input.mousePosition)) {
+//	            Debug.Log("upper Left!");
+				
+	        } else if (lowerLeftBounds.Contains(Input.mousePosition)) {
+//				Debug.Log("lower Left!");
+			} else if (upperRightBounds.Contains(Input.mousePosition)) {
+//				Debug.Log("upper Right!");
+			} else if (lowerRightBounds.Contains(Input.mousePosition)) {
+//				Debug.Log("lower Right!");
+			}
+		}
+	}
 	
 	void ResetPosition() {
 		transform.position = startPosition;
