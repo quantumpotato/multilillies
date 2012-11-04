abc019d3083a825d23ce9720a3087e09edf73947
Joshua Rieken <joshua@joshuarieken.com>
2012-11-03 20:05:23 -0500

    FISH!

--- Assets/Code/Fish.cs
+++ Assets/Code/Fish.cs
@@ -2,27 +2,48 @@ using UnityEngine;
 using System.Collections;
 
 public class Fish : Enemy {
+	public int pointCountMin = 5;
+	public int pointCountMax = 20;
+	public int pathLength = 40;
+	public float pointDeviation = 4.0f;
 	
 	private Vector3[] path;
+	private Vector3 rootPosition;
+	
+	private int pointCount;
+	private int PointCount {
+		get {
+			if (pointCount == 0) {
+				pointCount = Random.Range (pointCountMin, pointCountMax);
+			}
+			return pointCount;
+		}
+	}
 	
 	protected override void Start () {
 		base.Start();
+		
+		rootPosition = transform.position;
 		
 		GeneratePath();
 		Move();
 	}
 	
-	protected override void Update() {
-		base.Update();
-		
-		transform.position = new Vector3(transform.position.x + _speed * Time.deltaTime, transform.position.y, transform.position.z);
-	}
-	
 	void GeneratePath() {
+		path = new Vector3[PointCount + 2];
+		float pointGap = pathLength / PointCount;
 		
+		path[0] = rootPosition;
+		path[PointCount + 1] = new Vector3(rootPosition.x + (pathLength + pointGap), rootPosition.y, rootPosition.z);
+		for (int i = 1; i < PointCount + 1; i++) {
+			pointDeviation = -pointDeviation;
+			float randomZ = rootPosition.z + pointDeviation;
+			float newX = rootPosition.x + (pointGap*i);
+			path[i] = new Vector3(newX, rootPosition.y, randomZ);
+		}
 	}
 	
 	void Move() {
-		//iTween.MoveTo(gameObject, iTween.Hash("path", path, "time", 3));
+		iTween.MoveTo(gameObject, iTween.Hash("path", path, "time", Speed, "easetype", iTween.EaseType.linear));
 	}
 }
