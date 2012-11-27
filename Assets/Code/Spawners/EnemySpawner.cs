5fe7eaf8bf39d431c8ac14c7863cad765d32577b
quantumpotato <quantumpotato@gmail.com>
2012-11-27 11:08:47 -0600

    Reverted spawns

--- Assets/Code/Spawners/EnemySpawner.cs
+++ Assets/Code/Spawners/EnemySpawner.cs
@@ -124,9 +124,6 @@ public class EnemySpawner : MonoBehaviour {
 		sharkWeight = lightWeight;
 		
 		fishWeight = heavyWeight;
-		fishWeight = 100;
-		
 		bigFishWeight = fishWeight / 2;
-		bigFishWeight = 100;
 	}
 }
