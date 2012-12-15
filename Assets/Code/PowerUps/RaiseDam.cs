a5239b00996c2643cbcb3f321b465eb34fc134ba
Joshua Rieken <joshua@joshuarieken.com>
2012-12-15 17:22:32 -0600

    Reorganize some code files

--- Assets/Code/PowerUps/RaiseDam.cs
+++ Assets/Code/PowerUps/RaiseDam.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 using System.Collections;
 
 public class RaiseDam : PowerUp {
+	public RaiseDam():
+		base()
+	{
+		Name = "Dam";
+	}
+	
 	public override void ApplyTo(Frog frog) {
 		Dam.Instance.Raise();
 	}
--- Assets/Code/PowerUps/SummonFisherman.cs
+++ Assets/Code/PowerUps/SummonFisherman.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 using System.Collections;
 
 public class SummonFisherman : PowerUp {
+	public SummonFisherman():
+		base()
+	{
+		Name = "Fisherman";
+	}
+	
 	public override void ApplyTo(Frog frog) {
 		Fisherman.Instance.StartSequence();
 	}
