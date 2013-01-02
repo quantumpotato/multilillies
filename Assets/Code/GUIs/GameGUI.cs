e9a25e9ac5931ba02f96becb56dfe420b63b48c5
quantumpotato <quantumpotato@gmail.com>
2013-01-02 13:59:58 -0600

    Drawing competitive scores

--- Assets/Code/GUIs/GameGUI.cs
+++ Assets/Code/GUIs/GameGUI.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 public class GameGUI : MonoBehaviour {
 	int displayedScore;
+	int[] individualDisplayedScores = new int[]{0,0,0,0};
 	int updateScoreDisplayTimer;
 	int scoreDisplayTimerReset;
 	
@@ -32,9 +33,39 @@ public class GameGUI : MonoBehaviour {
 	}
 	
 	void Update() {
+		UpdateDisplayedScores();
+	}
+	
+	#endregion
+	
+	void UpdateDisplayedScores() {
 		updateScoreDisplayTimer--;
 		if (updateScoreDisplayTimer <= 0) {
-			int amplifiedFrogScore = Frog.TotalScore * 100;
+			if (GameManager.Instance.IsCoopMode()) {
+				UpdateDisplayedCoopScore();
+			} else {
+				UpdateDisplayedCompetitiveScores();
+			}
+			ResetScoreDisplayTimer();
+		}
+		
+		
+	}
+	
+	void ResetScoreDisplayTimer() {
+		updateScoreDisplayTimer = scoreDisplayTimerReset;
+	}
+	
+	void DrawScore() {
+		int width = 130;
+		int height = 23;
+		GUI.Box(new Rect (Screen.width/2-width/2,10,width,height), "" + displayedScore + " / " + Frog.HighRating);
+	}
+	
+	#region coop
+	
+	void UpdateDisplayedCoopScore() {
+		int amplifiedFrogScore = Frog.TotalScore * 100;
 			if (amplifiedFrogScore > displayedScore) {
 				if (amplifiedFrogScore > displayedScore + 27) {
 					displayedScore+= 9;	
@@ -48,23 +79,7 @@ public class GameGUI : MonoBehaviour {
 					displayedScore--;
 				}
 			}
-			ResetScoreDisplayTimer();
-		}
 	}
-	
-	#endregion
-	
-	void ResetScoreDisplayTimer() {
-		updateScoreDisplayTimer = scoreDisplayTimerReset;
-	}
-	
-	void DrawScore() {
-		int width = 130;
-		int height = 23;
-		GUI.Box(new Rect (Screen.width/2-width/2,10,width,height), "" + displayedScore + " / " + Frog.HighRating);
-	}
-	
-	#region coop
 	
 	void DrawLives() {
 		int width = 80;
@@ -83,7 +98,21 @@ public class GameGUI : MonoBehaviour {
 	#region competitive
 	
 	void DrawIndividualScores() {
-		
+		int width = 70;
+		int height = 30;
+		int x = leftmostScoreColumnX;
+		GUIStyle style = new GUIStyle();
+		style.fontSize = 30;
+		GUI.backgroundColor = Color.clear;
+		for (int i = 0; i < 4; i++) {
+			style.normal.textColor = Frog.FrogColors[i];
+			Frog f = PlayerManager.Instance.Frogs[i];
+			if (f.gameObject.active) {
+				GUI.color = Frog.FrogColors[i];
+				GUI.Box (new Rect(x,40,width, height), "" + individualDisplayedScores[i], style);
+			}
+			x+= scoreHorizontalMargin;
+		}
 	}
 	
 	void DrawIndividualMultipliers() {
@@ -101,6 +130,24 @@ public class GameGUI : MonoBehaviour {
 				GUI.Box (new Rect(x,Screen.height-40,width, height), "X" + f.scoreMultiplier, style);
 			}
 			x+= scoreHorizontalMargin;
+		}
+	}
+	
+	void UpdateDisplayedCompetitiveScores() {
+		for (int i = 0; i < 4; i++) {
+			Frog f = PlayerManager.Instance.Frogs[i];
+			if (f.gameObject.active) {
+				int amplifiedFrogScore = f.score * 100;
+				if (amplifiedFrogScore > individualDisplayedScores[i]) {
+					if (amplifiedFrogScore > individualDisplayedScores[i] + 27) {
+						individualDisplayedScores[i]+= 27;	
+					} else {
+						individualDisplayedScores[i]++;
+					}
+				} else if (amplifiedFrogScore < individualDisplayedScores[i]) {
+					individualDisplayedScores[i]--;	
+				}
+			}
 		}
 	}
 	
